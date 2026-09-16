2a39f92f56d1a04ae013af63da816e153e88a936
gdoyle-bit <gdoyle@rrc.academic.ca>
2026-09-16 11:44:49 -0500

    Vite and Express initialized

---
+++ server/app.ts
@@ -0,0 +1,12 @@
+import express, {type Express, type Request, type Response } from 'express';
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
