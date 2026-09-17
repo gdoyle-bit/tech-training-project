02c2f67cfdde0fd4797a51f5358559ed45dcb30c
gdoyle-bit <gdoyle@rrc.academic.ca>
2026-09-17 13:34:25 -0500

    Updated server/app.ts

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,30 @@
-import express, {type Express, type Request, type Response } from 'express';
+import "dotenv/config";
+
+import express, {
+  type Express,
+  type Request,
+  type Response,
+} from "express";
+
+import cors from "cors";
 
 const app: Express = express();
-const port: number = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+const port = Number(process.env.PORT) || 3000;
+const clientUrl = process.env.CLIENT_URL || "http://localhost:5173";
+
+app.use(
+  cors({
+    origin: clientUrl,
+  }),
+);
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Recipe Manager API is running!");
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  console.log(`Server listening on http://localhost:${port}`);
 });
